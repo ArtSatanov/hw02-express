60e5a6b5958716c297914fdc62b9c9f0b35284f7
ArtSatanov <artsatanov@gmail.com>
2023-12-25 21:37:25 +0200

    1

---
+++ controllers/contacts-controllers.js
@@ -0,0 +1,69 @@
+import * as contactService from '../models/contacts.js';
+import { HttpError } from '../helpers/index.js';
+import { contactAddSchema } from '../schemas/contacts-schemas.js';
+import { rules } from 'eslint-config-prettier';
+
+export const getAll = async (req, resp, next) => {
+  try {
+    const result = await contactService.listContacts();
+    resp.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getById = async (req, resp, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactService.getContactById(id);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    resp.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const add = async (req, resp, next) => {
+  try {
+    const error = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactService.addContact(req.body);
+    resp.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateById = async (req, resp, next) => {
+  try {
+    const error = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contactService.updateContact(id, resp.body);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    resp.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteById = async (req, resp, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactService.removeContact(id);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    resp.status(200).json({ message: 'Delete success' });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ helpers/HttpError.js
@@ -0,0 +1,15 @@
+const msgList = {
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+};
+
+const HttpError = (status, message = msgList[status]) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
+
+export default HttpError;
