510970e987d6b46e4cd306fcab07f4b853b85b63
ArtSatanov <artsatanov@gmail.com>
2024-01-06 19:11:14 +0200

    1

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,13 +1,20 @@
 import { HttpError } from '../helpers/index.js';
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 export const signup = async (req, resp, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw HttpError(409, 'Email in use');
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ ...req.body, password: hashPassword });
     if (!newUser) {
       throw HttpError(404);
     }
-    resp.json({
+    resp.status(201).json({
       email: newUser.email,
       subscription: newUser.subscription,
     });
@@ -16,6 +23,6 @@ export const signup = async (req, resp, next) => {
   }
 };
 
-export const signin = () => {
+export const signin = async (req, resp, next) => {
   return console.log('hello');
 };
